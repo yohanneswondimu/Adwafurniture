48c7e87c5b5fa95e4b765e21733ab9c36154953a
yohanneswondimu <yohanneswondimuq@gmail.com>
2024-12-05 22:27:46 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/Adwafurniture/', // Set the base URL for your project
+})
